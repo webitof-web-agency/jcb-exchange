664f32a1e223d470777c7d9c17ce4ce88b28470f
shubham001verma <sshubham123verma@gmail.com>
2026-09-03 14:30:37 +0530

    feat: restore full android native project with Firebase google-services.json

---
+++ JCB-Exchange/android/app/src/main/java/com/serviceportalmobile/NotificationClearerModule.kt
@@ -0,0 +1,20 @@
+package com.serviceportalmobile
+
+import android.app.NotificationManager
+import android.content.Context
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class NotificationClearerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "NotificationClearer"
+    }
+
+    @ReactMethod
+    fun clearAll() {
+        val notificationManager = reactApplicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+    }
+}
